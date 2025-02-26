b0a2f78f44e873da7e359e4978333a7b2e7c2aac
Yuvraj <fcyuvraj18@gmail.com>
2025-02-27 04:02:40 +0530

    up

---
+++ main.js
@@ -0,0 +1,22 @@
+const person = {
+    name: "SHivansh",
+    id: 12,
+    age:14
+}
+console.log(person['name'])
+console.log(person.id)
+
+// const person = {
+//     name: "Alice"
+//   };
+  
+//   console.log(person.toString()); 
+//   console.log(person.hasOwnProperty("name"));
+// //   console.log(person.valueOf());
+
+// function golu() {
+// }
+
+console.log(String(null));
+
+const helo = [];;;;;
